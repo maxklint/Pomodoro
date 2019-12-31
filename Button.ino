cd35b46927cebc637839904dcea3ff2d57ad4104
Maxim Zaev <max.klint79@gmail.com>
2019-12-31 17:33:06 +0300

    Rewrite button processing without interrupts

--- Button.ino
+++ Button.ino
@@ -1,17 +1,11 @@
-#include <RingBuf.h>
-
 const int buttonPin = 3;
 const unsigned long buttonLongPressTime = 600;
 const unsigned long buttonDebounceTime = 50;
 const unsigned long buttonDoubleClickTime = 200;
 const unsigned long buttonEventDebounceTime = 500;
 
-RingBuf<unsigned long , 32> buttonBuf;
-RingBuf<InputEvent, 16> buttonOutBuf;
-
 void buttonInit() {
   pinMode(buttonPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonISR, RISING);
 }
 
 InputEvent buttonUpdate() {
@@ -19,41 +13,46 @@ InputEvent buttonUpdate() {
   static unsigned long ignoreEventsUntil = 0;
   static bool pending = false;
 
-  unsigned long timestamp;
-  while (buttonBuf.lockedPop(timestamp)) {
-    if (timestamp <= ignoreEventsUntil) continue;
-    if (timestamp - lastPress <= buttonDebounceTime) continue;
-    pending = true;
-    if (timestamp - lastPress <= buttonDoubleClickTime) {
-      buttonOutBuf.push(BTN_EV_DOUBLE);
-      ignoreEventsUntil = timestamp + buttonEventDebounceTime;
-      pending = false;
-    }
-    lastPress = timestamp;
-  }
+  static int state = digitalRead(buttonPin);
+  static int prevState = state;
 
   unsigned long now = millis();
+  unsigned long timestamp = 0;
+  InputEvent outEv = BTN_EV_NONE;
+
+  prevState = state;
+  state = digitalRead(buttonPin);
+
+  if (state && !prevState)
+    timestamp = now;
+
+  if (!state && prevState)
+    ignoreEventsUntil = now + buttonDebounceTime;
+
+  if (timestamp) {
+    if (timestamp > ignoreEventsUntil && timestamp - lastPress > buttonDebounceTime) {
+      pending = true;
+      if (timestamp - lastPress <= buttonDoubleClickTime) {
+        outEv = BTN_EV_DOUBLE;
+        ignoreEventsUntil = timestamp + buttonEventDebounceTime;
+        pending = false;
+      }
+      lastPress = timestamp;
+    }
+  }
+
   if (pending && now > ignoreEventsUntil) {
     pending = false;
-    ignoreEventsUntil = timestamp + buttonEventDebounceTime;
-    int buttonState = digitalRead(buttonPin);
-    if (buttonState == LOW && now - lastPress > buttonDoubleClickTime)
-      buttonOutBuf.push(BTN_EV_CLICK);
-    else if (buttonState == HIGH && now - lastPress > buttonLongPressTime)
-      buttonOutBuf.push(BTN_EV_LONG);
+    ignoreEventsUntil = now + buttonEventDebounceTime;
+    if (!state && now - lastPress > buttonDoubleClickTime)
+      outEv = BTN_EV_CLICK;
+    else if (state && now - lastPress > buttonLongPressTime)
+      outEv = BTN_EV_LONG;
     else {
       pending = true; // still pending
       ignoreEventsUntil = 0;
     }
   }
 
-  InputEvent outEv;
-  if (buttonOutBuf.pop(outEv))
-    return outEv;
-  else
-    return BTN_EV_NONE;
-}
-
-void buttonISR() {
-  buttonBuf.push(millis());
+  return outEv;
 }
